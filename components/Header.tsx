1368b9aa69eea49693e522a95a88ab089d9135ed
arturs.langenfelds@28stone.com <alangenfeld@gmail.com>
2022-10-26 18:54:43 +0300

    add dependencies https://portal.thirdweb.com/react

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import NavButton from "./NavButton";
+import { Bars3BottomRightIcon } from "@heroicons/react/24/solid";
 // import Image from "next/image";
 
 type Props = {};
 
 const Header = (props: Props) => {
   return (
-    <div className="flex justify-between">
+    <header className="grid grid-cols-2 md:grid-cols-5 justify-between items-center">
       <div className="flex items-center space-x-2">
         <img
           className="rounded-full h-20 w-20"
@@ -19,15 +20,20 @@ const Header = (props: Props) => {
         </div>
       </div>
 
-      <div>
-        <div className="bg-[#0A1F1C]">
-          <NavButton />
-          <NavButton />
+      <div className="hidden md:flex md:col-span-3 justify-self-center items-center rounded-md">
+        <div className="bg-[#0A1F1C] p-4 space-x-2">
+          <NavButton isActive title="Buy tickets" />
+          <NavButton title="Logout" />
         </div>
       </div>
 
-      <div>Right</div>
-    </div>
+      <div className="flex flex-col ml-auto text-right">
+        <Bars3BottomRightIcon className="h-8 w-8 mx-auto text-white cursor-pointer" />
+        <span className="md:hidden">
+          <NavButton title="Logout" />
+        </span>
+      </div>
+    </header>
   );
 };
 
--- components/NavButton.tsx
+++ components/NavButton.tsx
@@ -2,10 +2,19 @@ import React from "react";
 
 type Props = {
   title: string;
+  isActive?: boolean;
 };
 
-const NavButton = ({ title }: Props) => {
-  return <button>NavButton</button>;
+const NavButton = ({ title, isActive }: Props) => {
+  return (
+    <button
+      className={`${
+        isActive && "bg-[#036756]"
+      } hover:bg-[#036756] text-white font-semibold py-2 px-4 rounded`}
+    >
+      {title}
+    </button>
+  );
 };
 
 export default NavButton;
